4c2e0ae1e7c3223c0755dc41ed7bad70b18824c9
Alexei Gontar <f.gontar5@gmail.com>
2020-02-01 13:57:47 +0200

    Hydrate benchmark improvement

--- test/other-issues/hydrate-performance/hydrate-performance.ts
+++ test/other-issues/hydrate-performance/hydrate-performance.ts
@@ -1,39 +1,59 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
-import {Post} from "./entity/Post";
+import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
+import { Connection, DatabaseType } from "../../../src";
+import { Post } from "./entity/Post";
+import { fail } from "assert";
+
+async function butchInsert(connection: Connection, posts: Post[], maxButchSize: number): Promise<void> {
+    if (maxButchSize < 100) {
+        console.warn("Suspicious butch size: ", maxButchSize);
+    }
+    let postsLeft = posts;
+    while (postsLeft.length > 0) {
+        await connection.manager.insert(Post, postsLeft.slice(0, maxButchSize));
+        postsLeft = postsLeft.slice(maxButchSize);
+    }
+}
 
 describe("other issues > hydration performance", () => {
 
     let connections: Connection[];
+    const enabledDrivers: DatabaseType[] = ["mysql", "postgres"];
     before(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
-        enabledDrivers: ["mysql"]
+        enabledDrivers,
     }));
     beforeEach(() => reloadTestingDatabases(connections));
     after(() => closeTestingConnections(connections));
+    describe("simple entity hydration", () => {
+        enabledDrivers.forEach((driverName) => {
+           it(driverName, async () => {
+               const connection = connections.find((it) => it.name === driverName);
+               if (!connection) {
+                   fail(`Connection with name ${driverName} not found`);
+                   return;
+               }
 
-    it("if entity was changed in the listener, changed property should be updated in the db", () => Promise.all(connections.map(async function(connection) {
+               // insert few posts first
+               const posts: Post[] = [];
+               for (let i = 1; i <= 100000; i++) {
+                   posts.push(new Post("Post #" + i));
+               }
+               const butchSize = connection.name === "postgres" ? 34464 : posts.length;
+               await butchInsert(connection, posts, butchSize);
 
-        // insert few posts first
-        const posts: Post[] = [];
-        for (let i = 1; i <= 100000; i++) {
-            posts.push(new Post("Post #" + i));
-        }
-        await connection.manager.insert(Post, posts);
+               // select them using raw sql
+               console.time(`select using raw sql ${connection.name}`);
+               const loadedRawPosts = await connection.manager.query("SELECT * FROM post");
+               loadedRawPosts.length.should.be.equal(100000);
+               console.timeEnd(`select using raw sql ${connection.name}`);
 
-        // select them using raw sql
-        // console.time("select using raw sql");
-        const loadedRawPosts = await connection.manager.query("SELECT * FROM post");
-        loadedRawPosts.length.should.be.equal(100000);
-        // console.timeEnd("select using raw sql");
-
-        // now select them using ORM
-        // console.time("select using ORM");
-        const loadedOrmPosts = await connection.manager.find(Post);
-        loadedOrmPosts.length.should.be.equal(100000);
-        // console.timeEnd("select using ORM");
-
-    })));
-
+               // now select them using ORM
+               console.time(`select using ORM ${connection.name}`);
+               const loadedOrmPosts = await connection.manager.find(Post);
+               loadedOrmPosts.length.should.be.equal(100000);
+               console.timeEnd(`select using ORM ${connection.name}`);
+           });
+        });
+    });
 });
